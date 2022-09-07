455f511179d22482f483b381fa93ba9ed10652a4
Guru Prasad <guruprasad@Gurus-Air.bbrouter>
2022-09-07 14:43:39 +0530

    splashedited

--- lib/View/Screens/SplashScreen/SplashScreen.dart
+++ lib/View/Screens/SplashScreen/SplashScreen.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:wudgres_admin_final/Utils/Image_Constrants.dart';
+import 'package:wudgres_admin_final/View/Screens/LoginScreen/LoginScreen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,12 +12,27 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+setTimer()
+{
+  Timer(Duration(seconds: 3), () {
+
+    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (c)=>LoginScreen()), (route) => false);
+
+  });
+}
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setTimer();
+  }
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Scaffold(
         body: Container(
-          color: Colors.black,
+          // color: Colors.black,
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
           child: Center(child: Image.asset(ImageConstrants.APPLOGO)),
